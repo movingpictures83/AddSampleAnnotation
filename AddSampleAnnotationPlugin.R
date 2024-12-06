0515fc31091cd5c7722372911284cb1ef3edd499
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2024-12-05 21:41:09 -0500

    Initial

---
+++ AddSampleAnnotationPlugin.R
@@ -0,0 +1,26 @@
+library(lipidr)
+library(ggplot2)
+
+dyn.load(paste("RPluMA", .Platform$dynlib.ext, sep=""))
+source("RPluMA.R")
+source("RIO.R")
+
+input <- function(inputfile) {
+  parameters <<- read.table(inputfile, as.is=T);
+  rownames(parameters) <<- parameters[,1];
+    pfix = prefix()
+  if (length(pfix) != 0) {
+     pfix <<- paste(pfix, "/", sep="")
+  }
+
+}
+
+run <- function() {}
+
+output <- function(outputfile) {
+  d <- readRDS(paste(pfix, parameters["data", 2], sep="/"))
+  
+d <- add_sample_annotation(d, paste(pfix, parameters["clinical", 2], sep="/"))
+saveRDS(d, outputfile)
+}
+
